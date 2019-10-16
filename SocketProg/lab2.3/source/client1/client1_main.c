f4dd03159af925b3eb96c999bc34f48f5f9ea6c6
eskadmas <eskad2004@gmail.com>
2019-10-16 11:05:48 +0200

    Socket Programming in c

---
+++ SocketProg/lab2.3/source/client1/client1_main.c
@@ -0,0 +1,198 @@
+/* 				ESKADMAS AYENEW TEFERA - s236174
+
+	The Client that requests the transfer of file(s), whose name(s) is/are specified on the
+	command line as 3rd and/or subsequent parameter(s), and stores them locally in its working
+	Directory. The Client prints the following line of texts to the standard output:
+		- The received file name
+		- The received file size (in bytes) and
+		- The received file's timestamp of last modification (as a decimal number)	 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <signal.h>
+
+#include "../mysocket.h"
+#include "../errlib.h"
+#include "../sockwrap.h"
+
+#define BUFSIZE		500
+
+char *prog_name;
+
+int main(int argc, char *argv[])
+{
+
+	char     	t_buf[BUFSIZE];		/* Transmission Buffer */
+	char		r_buf[BUFSIZE];		/* Reception Buffer */
+	int		read_char;		/* Length of the received message */
+	uint32_t 	filesize;		/* The received file size in bytes */
+	uint32_t 	lastmod;		/* The timestamp of the file */
+	uint32_t 	tot_char_read;
+	fd_set 		readset; 
+	FILE 		*fd;			/* fd refers the received file that is going to be written in Client Directory */
+	char 		filename[BUFSIZE];	
+	short		port;
+	int		s, result;		
+	uint32_t 	taddr_n;
+	uint16_t 	tport_n, tport_h;
+	struct 		sockaddr_in saddr;	/* Server address structure */	
+	struct 		in_addr sIPaddr;
+	int 		len, number_of_file, remaining_file;		
+	uint32_t        tmp;
+	
+	prog_name = argv[0];
+
+	/* Check Arguments */
+	
+	if(argc < 4){
+		err_msg("usage: <address> <port> <filename> [<filename>...] \n");
+		exit(1);
+	}
+	
+	result=inet_aton(argv[1], &sIPaddr);
+	if(!result){
+		printf("(%s) Invalid Server IP Address.\n", prog_name);
+		exit(1);
+	}
+	
+	if(sscanf(argv[2], "%" SCNu16, &tport_h)!=1) /* sscanf() reads formatted input from a string */
+	{ 
+		printf("Invalid port number \n");
+		exit(1);
+	}
+	tport_n=htons(tport_h);
+
+	/* Create Socket */
+	
+	s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(s < 0){
+		err_msg("Socket() failed.\n");
+		exit(1);
+	}
+	else
+		printf("[%s] Socket created: %d \n", prog_name, s);
+
+	/* Specify Address to Bind */
+	
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sin_family= AF_INET;
+	saddr.sin_port= tport_n;
+	saddr.sin_addr= sIPaddr;
+
+	result = connect(s,(struct sockaddr *) &saddr, sizeof(saddr));
+	if(result == -1)
+	{ 
+		err_msg("Connect() failed \n");
+	   	close(s);
+		exit(1);
+	}
+	else
+		printf("[%s] Connection successfull \n", prog_name);
+
+	/* Main Client Loop*/
+	
+	number_of_file = argc - 3;		/* arguments other than prog_name, ip, and port is/are filename(s) */
+	remaining_file = number_of_file;	/* At first the number of remaining files is equal to the number of files */
+	int i = 0;				/* i refers the position of the file */
+	while(remaining_file > 0)
+	{
+		strcpy(t_buf, "GET ");
+		strcat(t_buf, argv[i+3]); 
+		strcat(t_buf, "\r\n");
+
+		len = strlen(t_buf);
+		if(writen(s, t_buf, len) != len){
+			printf("Write error \n");
+			exit(1);
+		}
+		
+		read_char = recv(s, r_buf, BUFSIZE, 0);
+		
+		if (read_char < 0)
+		{
+			printf("[%s] recv() error\n", prog_name);
+			exit(1);
+		}
+		
+		if (strncmp(r_buf, "-ERR\r\n", 6) == 0)
+		{
+			printf("[%s] Error message received from server \n", prog_name);
+			printf("====================================\n");
+		}
+		
+		if (strncmp(r_buf, "+OK\r\n", 5) == 0)
+		{
+			memcpy(&tmp, &r_buf[5], 4);
+			filesize = ntohl(tmp);
+			memcpy(&tmp, &r_buf[9], 4);	
+			lastmod = ntohl(tmp);
+			
+			strcpy(filename, argv[i+3]);
+	
+			fd = fopen(filename, "w");		/* The file received and to be written in the Client directory */
+			if (fd == NULL)
+				printf("[%s] - file %s cannot be opened \n", prog_name, filename);			
+			else
+			{
+				if (fwrite(&r_buf[13], sizeof(char), (read_char - 13), fd) < (read_char - 13))	/* Write the first packet in the file */
+					printf("[%s] - error in writing a file\n", prog_name);
+					
+				tot_char_read = filesize - (read_char - 13); //tot_char_read refers the remaining total content to be read 
+		
+				while(tot_char_read>0)
+				{
+					memset(r_buf, BUFSIZE,0);
+					if(tot_char_read> sizeof(r_buf))
+					{
+						read_char=recv(s, r_buf, sizeof(r_buf), 0);
+						r_buf[sizeof(r_buf)] = '\0';
+					}
+					else
+					{
+						read_char=recv(s, r_buf, tot_char_read, 0);
+						r_buf[tot_char_read]='\0';
+					}
+					if(read_char>0)			/* Check if the packet is received correctly */
+					{
+						tot_char_read -=read_char;
+						fwrite(r_buf, sizeof(char), read_char, fd);
+					}									   
+					else
+					{ 
+						printf("error\n");
+						break;
+	  				}
+				}
+			}
+			fclose(fd);
+			
+			printf("[%s] -- File[%d] information: \n", prog_name, i); 
+			printf("		Received file name: %s \n", filename); 
+			printf("		Received file size: %u \n", filesize);   
+			printf("		Received file last modification time: %u \n", lastmod);
+	
+		}
+		remaining_file--;
+		i++;
+		memset(r_buf, BUFSIZE,0);
+	}
+	
+	if(writen(s, "QUIT\r\n", 6) != 6)
+	
+		printf("Write error\n");
+		
+	closesocket(s);
+	exit(1);
+}
+
+
---
+++ SocketProg/lab2.3/source/server2/server2_main.c
@@ -0,0 +1,261 @@
+/* 				ESKADMAS AYENEW TEFERA - s236174
+
+		CONCURRENT TCP SERVER THAT TRANSFERS FILES
+		
+	The Server that accepts file transfer request(s) from the client and sends the requested file(s) back to the client.
+	The server must create processes on demand (a new process for each new TCP connection).  
+	
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <signal.h>
+#include <sys/wait.h>
+
+#include "../mysocket.h"
+#include "../errlib.h"
+#include "../sockwrap.h"
+
+#define BUFSIZE		500
+#define FILELEN		132
+
+char *prog_name;
+int serve_client(int con_req_receiver_skt);
+
+int main(int argc, char *argv[])
+{
+
+	int			con_req_receiver_skt;	/* The Socket at which the Server accepts Client requests */
+	uint16_t		lport_n, lport_h;	/* The port where the server listens(net/host byte order response) */
+	int			bklog = 10; 		/* Listening backlog */
+	int			s;			/* The Socket at the server*/
+	int 			result, addrlen;
+	struct sockaddr_in	saddr, caddr;		/* Server and client address structures */
+	short 			port;
+
+	prog_name=argv[0];
+
+	/* Check Arguments */
+	
+	if(argc != 2){
+		err_msg("usage: (%s) <port>\n", prog_name);
+		exit(1);
+	}
+	port=strtol(argv[1],NULL,0);	
+
+	/* Creating Socket */
+	
+	s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(s < 0){
+		err_msg("Socket() failed!\n");
+		exit(1);
+	}
+	printf("[%s] Socket created: %u \n", prog_name, s); 
+
+	/* Bind Socket to any local IP address */
+	
+	saddr.sin_family=AF_INET;
+	saddr.sin_port=htons(port);
+	saddr.sin_addr.s_addr=INADDR_ANY;
+
+	result=bind(s, (struct sockaddr *)&saddr, sizeof(saddr));
+	if(result == -1){
+		err_msg("Bind() failed! \n");
+		exit(1);
+	}
+	
+	/* Listen */
+	
+	result=listen(s, bklog);
+	if(result == -1){
+		err_msg("Listen() failed! \n");
+		exit(1);
+	}
+	printf("[%s] Listening to client request. . .  \n", prog_name);	
+		
+	/* Main Server Loop */
+	
+	for(;;)
+	{
+	
+		/* Accept Client requests */
+		
+		addrlen = sizeof(struct sockaddr_in);
+		con_req_receiver_skt = accept(s, (struct sockaddr *) &caddr, &addrlen);
+		
+		if(con_req_receiver_skt < 0){
+			err_quit("Accept() failed! \n");
+			exit(1);
+		}
+		printf("[%s] New Socket: %u\n", prog_name, con_req_receiver_skt);
+		
+		/* Serve the client on socket s */
+		
+		/* Fork a new process to serve the client on the new connection */
+		
+		int childpid;
+		if((childpid=fork()) < 0) 
+		{ 
+			err_msg("Fork() failed! \n");
+			close(con_req_receiver_skt);
+		}
+		else if (childpid > 0)
+		{ close(con_req_receiver_skt);		/* close connected socket */
+			waitpid(childpid, NULL, 0);			
+		}
+		else
+		{
+			/* Child process */ 
+			close(s);				/* close listening socket */
+			serve_client(con_req_receiver_skt);	/* serve client */ 
+           		exit(1);
+		}
+	}
+	return 0;
+}
+
+int serve_client(int con_req_receiver_skt)
+{
+
+	char			filename[FILELEN];	/* The name of the requested file */
+	char			t_buf[BUFSIZE];		/* Transmission Buffer */
+	char			r_buf[BUFSIZE];		/* Reception Buffer */		
+	char			file[FILELEN];		/* The contents of a file */
+	int 			sent_char;		/* Length of the sent message */
+	int 			read_char;		/* Length of the received message */
+	struct stat 		statbuf;
+	uint32_t 		filesize;
+	uint32_t 		lastmod;
+	uint16_t		lport_n, lport_h;	/* The port where the server listens(net/host byte order response) */
+	int 			result, addrlen, n;
+	FILE			*fd;
+	
+        for(;;)
+	{	                       
+		read_char = recv(con_req_receiver_skt, r_buf, BUFSIZE, 0);
+		if(read_char < 0){
+			printf("Read error \n");
+			close(con_req_receiver_skt);
+			break;
+		}
+		else if(read_char == 0){
+			printf("Connection closed by party on socket %u \n", con_req_receiver_skt);
+			close(con_req_receiver_skt);
+			break;
+		}
+		else
+		{
+			if (strncmp(r_buf, "QUIT\r\n", 6) == 0)		/* Client request to quit the connection */
+			{
+				printf("[%s] -- QUIT command received from Client -- \n", prog_name);
+				close(con_req_receiver_skt);
+				break;
+			}
+			else if(strncmp(r_buf, "GET ", 4)==0)		/* A command for Client file request */
+			{
+				int i;		
+				for(i=0; i<strlen(r_buf)&& r_buf[i] != '\r'; i++)
+					continue;
+					
+				i -=4;
+				char *filename;
+				filename = (char *) malloc(sizeof(char) * (i+1));
+				strncpy(filename, &r_buf[4], i);
+				filename[i] = '\0';
+				
+				printf("\n[%s] The requested file is: %s\n", prog_name, filename);
+
+				fd = fopen(filename, "r");		/* Open file */
+				if (fd == NULL)
+				{
+					printf("[%s] -- file not found -- \n", prog_name);
+						
+					if (send(con_req_receiver_skt, "-ERR\r\n", 6, 0) != 6)
+	    					printf("[%s] -- error in sending response \n", prog_name);
+					continue;
+				}
+				else
+				{
+					/* Get file last modification time */
+					
+					struct stat st;
+
+					if (stat(filename, &st)) 
+						perror(filename);
+						
+					else					
+						printf("[%s] [%s] lastmod time is: %lld\n", prog_name, filename, (long long)st.st_mtim.tv_sec);				
+					lastmod = st.st_mtim.tv_sec;
+				
+					printf("[%s] -- opened for reading -- \n", filename);					
+					printf("[%s] -- sending file -- \n", prog_name);
+
+					/* Get file size */
+					
+					fseek (fd, 0, SEEK_END);   
+					filesize=ftell (fd);
+					rewind(fd);
+					snprintf(t_buf, 6, "+OK\r\n");				
+					uint32_t tmp = htonl(filesize);
+					memcpy(&t_buf[5], &tmp, 4);
+					tmp = htonl(lastmod);
+					memcpy(&t_buf[9], &tmp, 4);
+							
+					sent_char = fread(&t_buf[13], sizeof(char), BUFSIZE-13, fd);	/* Read the file in the buffer */
+					
+					if (send(con_req_receiver_skt, t_buf, sent_char + 13, 0) != (sent_char + 13))/* Send the first packet */
+					{
+						printf("[%s]--error in sending a packet \n", prog_name);
+						close(con_req_receiver_skt);
+						break;
+					}
+
+					/* Send the rest of the file */
+					
+					bzero(t_buf, BUFSIZE); 
+					uint32_t sentbytes = BUFSIZE - 13;	/* The file content */
+					while ((sent_char = fread(t_buf, sizeof(char), BUFSIZE, fd)) > 0)	
+					{
+						if (send(con_req_receiver_skt, t_buf, sent_char, 0) != sent_char)
+						{
+							printf("[%s] -- error in sending a file \n", prog_name);
+							close(con_req_receiver_skt);
+							break;
+						}
+						sentbytes += sent_char;
+						bzero(t_buf, BUFSIZE);
+					}
+					printf("[%s] -- file has sent --\n", prog_name);
+					fclose(fd);
+				}
+			}
+			
+			/* Handle invalid command */
+
+			else	
+			{
+				printf("[%s] -- invalid command: %s\n", prog_name, r_buf);
+
+				if (send(con_req_receiver_skt, "-ERR\r\n", 6, 0) != 6)
+	    				printf("[%s] -- error in sending a message\n", prog_name);
+
+				printf("[%s] Closing Socket %d ...\n", prog_name, con_req_receiver_skt);
+				continue;
+			}
+		}
+		
+	}
+	free(r_buf);
+	free(t_buf);
+	return 0;
+}
+			/* Parent process */ 
+			
